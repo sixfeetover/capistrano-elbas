630c291e18faffe88e5f88537e0377eddc7d2547
James Bloomer <github2@thebloomers.co.uk>
2017-05-17 15:01:23 +0100

    Use the public DNS if available
    This brings it back in line with the original source before the multi
    ASG change.

--- lib/elbas/capistrano.rb
+++ lib/elbas/capistrano.rb
@@ -18,7 +18,7 @@ def autoscale(groupname, *args)
       p "ELBAS: Skipping unhealthy instance #{instance.id}"
     else
       ec2_instance = ec2_resource.instance(asg_instance.id)
-      hostname = ec2_instance.private_ip_address
+      hostname = ec2_instance.public_dns_name || ec2_instance.private_ip_address
       p "ELBAS: Adding server: #{hostname}"
       server(hostname, *args)
     end
